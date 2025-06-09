2e95389e1f6ada19fc23e59484aca9cfa2d81dee
devjoshdev <jsinanan1@gmail.com>
2025-06-09 01:30:09 -0400

    Grab food name and calories for one food item

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ import puppeteer from "puppeteer";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(MCDONALDS_FULL_MENU, {waitUntil: "networkidle0"});
-    
+    const foodLinkSelector = await page.$$("a[href^='/us/en-us/product/']");
+    for (let i = 0; i < foodLinkSelector.length; i++) {
+        const foodLink = await foodLinkSelector[i].evaluate(elem => elem.href);
+        console.log(i, foodLink);
+        await page.goto(foodLink, {waitUntil: "networkidle0"});
+        const foodNameSpan = await page.$("span.cmp-product-details-main__heading-title");
+        const foodName = await foodNameSpan.evaluate(elem => elem.innerText);
+        const calorieDivSelector = await page.$("div.cmp-product-details-main__sub-heading");
+        console.log(calorieDivSelector);
+        const calories = await calorieDivSelector.evaluate(elem => {
+            console.log("here");
+            const calorieSpan = elem.firstChild;
+            console.log("calorie span=>", calorieSpan);
+            return calorieSpan.innerText;
+        });
+        console.log("name:", foodName, ",calories:", calories);
+        process.exit(0);
+    }
 
+    await browser.close();
 })();

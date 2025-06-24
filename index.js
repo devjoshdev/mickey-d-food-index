1cadc46baaedec8a6496132393bcc45d9f6887e9
devjoshdev <jsinanan1@gmail.com>
2025-06-24 01:06:00 -0400

    More progress and using  instead of evaluate for better reliability

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ import puppeteer from "puppeteer";
 
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    });
     await page.goto(MCDONALDS_FULL_MENU, {waitUntil: "networkidle0"});
     const productPageLinks = await page.$$eval("a[href^='/us/en-us/product/']", (anchors => anchors.map(anchor => anchor.href)));
     let otherSizesLinks = [];
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < productPageLinks.length; i++) {
         await page.goto(productPageLinks[i], {waitUntil: "networkidle0"});
         const foodNameAndCalories = await extractFoodNameAndCalories(page);
         console.table(foodNameAndCalories);
@@ -32,14 +36,19 @@ import puppeteer from "puppeteer";
 
 // expects the page to be fully loaded before being passed to this function
 async function extractFoodNameAndCalories(page) {
-    const foodNameSpan = await page.$("span.cmp-product-details-main__heading-title");
-    const foodName = await foodNameSpan.evaluate(elem => elem.innerText);
-    const calorieDivSelector = await page.$("div.cmp-product-details-main__sub-heading");
-    const calories = await calorieDivSelector.evaluate(elem => {
-        const calorieSpan = elem.firstChild;
-        return calorieSpan.innerText.trim();
+    let nameCals = {
+        foodName: "",
+        calories: ""
+    }
+    nameCals = await page.$eval("span.cmp-product-details-main__heading-title", elem => {
+        const foodName = elem.innerText;
+        const calories = document.querySelector("div.cmp-product-details-main__sub-heading")?.firstChild?.innerText?.trim();
+        return {
+            foodName: foodName,
+            calories: calories,
+        }
     });
-    return {"foodName": foodName, "calories": calories};
+    return nameCals;
     
 }
 
